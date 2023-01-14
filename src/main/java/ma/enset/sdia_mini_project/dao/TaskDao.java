607fe7ad03e9b6952f0a5b309b6d366ac2b50c71
abdlkrim3 <abdlkrim.agoujil@gmail.com>
2023-01-15 00:36:05 +0100

    creat Dao for Task

--- src/main/java/ma/enset/sdia_mini_project/dao/TaskDao.java
+++ src/main/java/ma/enset/sdia_mini_project/dao/TaskDao.java
@@ -1,2 +1,6 @@
-package ma.enset.sdia_mini_project.dao;public interface TaskDao {
+package ma.enset.sdia_mini_project.dao;
+
+import ma.enset.sdia_mini_project.dao.entities.Task;
+
+public interface TaskDao extends Dao<Task>{
 }
