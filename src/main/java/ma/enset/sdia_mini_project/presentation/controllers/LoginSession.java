97391b6deb7d1dddc1072c704ef8a24ae67e438f
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-01-24 00:22:08 +0100

    Added session

---
+++ src/main/java/ma/enset/sdia_mini_project/presentation/controllers/LoginSession.java
@@ -0,0 +1,10 @@
+package ma.enset.sdia_mini_project.presentation.controllers;
+
+public class LoginSession {
+
+    public static long id;
+    public static String UsetrType;
+    public static String f_Name;
+    public static String l_Name;
+    public static boolean isLoggedIn = false;
+}
