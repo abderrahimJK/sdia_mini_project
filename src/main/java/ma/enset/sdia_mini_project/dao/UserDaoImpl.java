a050e8fad935acfde02e8c3cb7ff3e1293bbb9c8
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-01-24 00:28:48 +0100

    added update user info

--- src/main/java/ma/enset/sdia_mini_project/dao/UserDaoImpl.java
+++ src/main/java/ma/enset/sdia_mini_project/dao/UserDaoImpl.java
@@ -3,16 +3,40 @@ package ma.enset.sdia_mini_project.dao;
 import ma.enset.sdia_mini_project.dao.UserDao;
 import ma.enset.sdia_mini_project.dao.entities.Project;
 import ma.enset.sdia_mini_project.dao.entities.User;
+import ma.enset.sdia_mini_project.presentation.controllers.PasswordUtils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
     @Override
     public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        Connection connection;
+        try{
+            connection = SingletonConnexionDB.getConnection();
+            String query = "select * from USER";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                User user  = new User();
+                user.setId(rs.getLong("ID"));
+                user.setF_Name(rs.getString("F_NAME"));
+                user.setL_Name(rs.getString("L_NAME"));
+                user.setEmail(rs.getString("EMAIL"));
+                user.setPassword(rs.getString("PASSWORD"));
+                user.setPhone(rs.getString("PHONE_NUMBER"));
+                user.setRole(rs.getString("ROLE"));
+                list.add(user);
+            }
+            return list;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -27,10 +51,11 @@ public class UserDaoImpl implements UserDao {
             if(rs.next()){
                 user.setId(rs.getLong("ID"));
                 user.setF_Name(rs.getString("F_NAME"));
-                user.setF_Name(rs.getString("L_NAME"));
-                user.setF_Name(rs.getString("EMAIL"));
-                user.setF_Name(rs.getString("PASSWORD"));
-                user.setF_Name(rs.getString("ROLE"));
+                user.setL_Name(rs.getString("L_NAME"));
+                user.setEmail(rs.getString("EMAIL"));
+                user.setPhone(rs.getString("PHONE_NUMBER"));
+                user.setPassword(rs.getString("PASSWORD"));
+                user.setRole(rs.getString("ROLE"));
             }
         }catch(SQLException e){
             e.printStackTrace();
@@ -63,7 +88,23 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    public User update(User o) {
+    public User update(User a) {
+        try{
+            System.out.println(a);
+            Connection connection = SingletonConnexionDB.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("Update USER SET F_NAME = ?,L_NAME = ?, Email = ?, PHONE_NUMBER = ?, PASSWORD = ? WHERE ID=?");
+            preparedStatement.setString(1,a.getF_Name());
+            preparedStatement.setString(2,a.getL_Name());
+            preparedStatement.setString(3,a.getEmail());
+            preparedStatement.setString(4,a.getPhone());
+            preparedStatement.setString(5,a.getPassword());
+            preparedStatement.setLong(6,a.getId());
+            preparedStatement.executeUpdate();
+
+            return a;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -71,10 +112,11 @@ public class UserDaoImpl implements UserDao {
     public User toLogin(String email, String password) {
         Connection connection=SingletonConnexionDB.getConnection();
         User user = new User();
+        String hashed_pwd = PasswordUtils.hashPassword(password);
         try{
             PreparedStatement stm=connection.prepareStatement("SELECT * FROM USER where EMAIL=? AND PASSWORD=?");
             stm.setString(1,email);
-            stm.setString(2,password);
+            stm.setString(2,hashed_pwd);
             ResultSet rs=stm.executeQuery();
             if(rs.next()){
                 user.setId(rs.getLong("ID"));
