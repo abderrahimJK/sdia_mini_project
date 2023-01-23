b3f6765d946e7b99f85bd2cf4b7290e937a271c3
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-01-24 00:28:28 +0100

    add logout action to the user switch cases

--- src/main/java/ma/enset/sdia_mini_project/presentation/controllers/client/UserController.java
+++ src/main/java/ma/enset/sdia_mini_project/presentation/controllers/client/UserController.java
@@ -2,6 +2,7 @@ package ma.enset.sdia_mini_project.presentation.controllers.client;
 
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import ma.enset.sdia_mini_project.presentation.models.Model;
 import ma.enset.sdia_mini_project.presentation.views.UserMenuOptions;
 
@@ -18,6 +19,10 @@ public class UserController implements Initializable {
             switch(newVal){
                 case PROFILE:user_parent.setCenter(Model.getInstance().getViewFactory().getProfileView());
                     break;
+                case LOGOUT: {
+                    Model.getInstance().getViewFactory().closeStage((Stage) user_parent.getScene().getWindow());
+                    Model.getInstance().getViewFactory().showLogin();
+                } break;
                 default: user_parent.setCenter(Model.getInstance().getViewFactory().getUserDashboard());
             }
         });
