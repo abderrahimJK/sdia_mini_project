9cd252d3d8ce9e26a59528e3c16f68ef24993cd2
abderrahimJK <abderrahim.aitbouna@gmail.com>
2023-01-24 00:21:38 +0100

    change textField to passwordField & hash the password

---
+++ src/main/java/ma/enset/sdia_mini_project/presentation/controllers/PasswordUtils.java
@@ -0,0 +1,22 @@
+package ma.enset.sdia_mini_project.presentation.controllers;
+
+import java.security.MessageDigest;
+
+public class PasswordUtils {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
